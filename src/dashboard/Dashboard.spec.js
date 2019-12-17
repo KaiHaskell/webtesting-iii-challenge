13918a1f90e938577fa196861dc0560d0b7cf7af
kai-haskell <me@kaihaskell.com>
2019-12-16 19:23:43 -0500

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+test("shows the controls and display", () => {
+  const { getByText } = render(<Dashboard />);
+  const display = getByText(/Open/i);
+  const controls = getByText(/Close Gate/i);
+  getByText(/Unlocked/i);
+
+  expect(display).toBeDefined;
+  expect(controls).toBeDefined;
+  expect(display).toMatchSnapshot();
+});
